c165fd187a78943db407f90b0ad693fa670e04dd
ChandravadiyaDeep <chandravadiyadeep593@gmail.com>
2026-07-22 17:05:38 +0530

    dsf

--- analysis/analyzer.py
+++ analysis/analyzer.py
@@ -2,9 +2,11 @@
 from analysis.component.dataset_summary import generate_summary
 from analysis.component.dataset_validator import generate_validation
 from analysis.component.dataset_statistics import generate_statistics
+from modules import generate_data_insights
 def analyze_dataset(df):
     analysis_report = {}
     analysis_report["summary"] = generate_summary(df)
     analysis_report["validation"] = generate_validation(df)
     analysis_report["statistics"] = generate_statistics(df)
+    analysis_report["data_insights"] = generate_data_insights(df)
     return analysis_report
--- analysis/modules/datainsights.py
+++ analysis/modules/datainsights.py
@@ -2,3 +2,6 @@ from analysis.component.dataset_summary import generate_summary
 from analysis.component.dataset_validator import generate_validation
 from analysis.component.dataset_statistics import generate_statistics
 
+
+def generate_data_insights(df):
+    insights = {}
