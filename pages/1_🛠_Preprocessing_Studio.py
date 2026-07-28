7ca91e8cdda7d9ff028c9ee87ce8da7457da8399
ChandravadiyaDeep <chandravadiyadeep593@gmail.com>
2026-07-28 08:31:55 +0530

    integrating the studio page

--- pages/1_🛠_Preprocessing_Studio.py
+++ pages/1_🛠_Preprocessing_Studio.py
@@ -8,4 +8,35 @@ st.set_page_config(
 
 st.title("🛠 Data Preprocessing Studio")
 
-st.write("Welcome to the preprocessing pipeline")
+st.divider()
+
+#dataset information
+col1,col2 =st.columns(2)
+
+with col1:
+    st.metric("Dataset","No Dataset")
+with col2:
+    st.metric("Pipeline Steps","0")
+
+st.divider()
+
+#main layout
+left, center , right=st.columns([1,2,1])
+#left panel
+with left:
+    st.subheader("⚙ Operations")
+
+    st.button("Missing Values",use_container_width=True)
+
+    st.button("Encoding",use_container_width=True)
+
+    st.button("Scaling",use_container_width=True)
+
+    st.button("Outliers",use_container_width=True)
+
+    st.button("Duplicates",use_container_width=True)
+
+    st.button("Features Selection",use_conatiner_width=True)
+
+    st.button("Data Type",use_conatainer_width=True)
+
