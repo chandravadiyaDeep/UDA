164a005d40e59291fcc6aa25c049fe3c0c63e905
ChandravadiyaDeep <chandravadiyadeep593@gmail.com>
2026-07-29 21:50:50 +0530

    integrating the preprocessing studio

--- analysis/modules/preprocessing/executor.py
+++ analysis/modules/preprocessing/executor.py
@@ -1,3 +1,15 @@
-def execute_pipeline(df, pipeline):
+from analysis.modules.preprocessing.operations import fill_missing
 
-    pass
+def execute_pipeline(df, pipeline):
+    processed_df=df.copy()
+    #get all the pipeline steps
+    steps=pipeline.get_steps()
+
+    for step in steps:
+        category=step["category"]
+
+        #missing value
+        if category == "Missing Values":
+            processed_df=fill_missing(processed_df,column=step["column"],method=step["method"])
+
+    return processed_df        
--- analysis/modules/preprocessing/operations.py
+++ analysis/modules/preprocessing/operations.py
@@ -1,10 +1,12 @@
+import pandas as pd
+
+
 OPERATIONS = {
 
     "Missing Values": [
         "Mean",
         "Median",
         "Mode",
-        "KNN",
         "Drop Rows"
     ],
 
@@ -43,3 +45,30 @@ OPERATIONS = {
         "Datetime"
     ]
 }
+
+#missing value operations
+
+def fill_missing(df,column,method):
+
+    processed_df = df.copy
+
+    #mean
+    if method == "mean":
+
+        processed_df[column]=processed_df[column].fillna(
+            processed_df[column].mean
+        )
+    #median
+    elif method == "Median":
+        processed_df[column]=processed_df[column].fillna(processed_df[column].median())
+    #mode
+    elif method == "Mode":
+
+        mode=processed_df[column].mode()
+
+        if not mode.empty:
+            processed_df[column]=processed_df[column].fillna(mode.iloc[0])
+    elif method == "Drop Rows":
+
+        processed_df=processed_df.dropna(subset=[column])
+    return processed_df                  
